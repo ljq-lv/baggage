f40ae6c65cffe94c9f0048c2d8a30cea46ac4e60
kk <kk@example.com>
2026-06-12 11:49:08 +0800

    feat: use high-resolution images on mobile web

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
   const SVG_NS = "http://www.w3.org/2000/svg";
   const RENDER_LIMIT = 200;
   const VIEW_ONLY = false;
-  const APP_VERSION = "20260612-v11";
+  const APP_VERSION = "20260612-v12";
   const FIXED_DRAWING_ORDER = ["f4", "f3", "f2", "f1", "b1", "f3-transfer", "overview-2d", "overview-3d"];
 
   const defaultDrawings = [
@@ -3175,7 +3175,7 @@
     if (!drawing) return;
     var imageSrc = IS_NATIVE_APP && drawing.nativeImage
       ? drawing.nativeImage
-      : (isCompactViewport() && drawing.mobileImage ? drawing.mobileImage : drawing.image);
+      : drawing.image;
     var prevSrc = el.image.getAttribute("src") || "";
     var srcChanged = prevSrc !== imageSrc;
     if (srcChanged) {
